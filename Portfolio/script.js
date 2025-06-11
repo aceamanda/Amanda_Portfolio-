f38ba294e3254ff27ac900e59cfeb865a71bf0e2
Amanda Bormann <amandacamposlima@gmail.com>
2025-06-10 22:44:23 -0300

    Update script.js

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,11 +1,11 @@
-const inicio = document.querySelector('.Inicio');
-const contatopag = document.querySelector('.contatoBnt');
-const voltar = document.querySelector('.voltarBnt');
+const inicioContainer = document.querySelector('.Inicio');
+const botaoParaContato = document.querySelector('.contatoBnt');
+const botaoParaVoltar = document.querySelector('.voltarBnt');
 
-contatopag.addEventListener('click', ()=> {
-    inicio.classList.add('active');
+botaoParaContato.addEventListener('click', () => {
+    inicioContainer.classList.add('active');
 });
 
-voltar.addEventListener('click', ()=> {
-    inicio.classList.remove('active');
+botaoParaVoltar.addEventListener('click', () => {
+    inicioContainer.classList.remove('active');
 });
